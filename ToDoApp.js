b8ece38771cd96da325b40afb781ebf630453c3b
Andrew Langford <a.b.l.2142@gmail.com>
2017-03-15 11:33:06 -0600

    ui updated

--- ToDoApp.js
+++ ToDoApp.js
@@ -1,15 +1,18 @@
 
 angular.module("toDoApp",["ui.router"]).config(function($urlRouterProvider,$stateProvider){
     $urlRouterProvider.when("","/");
+
     $stateProvider
+
         .state("home",{
             templateUrl:"home/home.html",
             url:"/"
-        }
+        })
+
             .state("todo",{
                 templateUrl:"to-do/toDo.html",
                 url:"toDo"
             })
-    )
 
-})
+
+});
